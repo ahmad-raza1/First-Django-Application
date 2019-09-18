a8b957969b6b42a4734dfdaf7198d7c024acaa8c
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-18 17:50:38 +0500

    Modified

--- helloapp/myApp/urls.py
+++ helloapp/myApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.index)
+	path('', views.Home),
+	path('about/', views.About),
 ]
--- helloapp/myApp/views.py
+++ helloapp/myApp/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+"""
 def index(request):
 	return HttpResponse("Hello, world!")
+"""
+
+def Home(request):
+	return render(request, 'index.html')
+
+def About(request):
+	return render(request, 'about.html')
